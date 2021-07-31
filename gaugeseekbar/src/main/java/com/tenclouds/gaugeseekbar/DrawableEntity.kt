701db5fd410739939a84fa1590a0334adeb92be5
Meet Mistry <>
2021-07-31 15:22:39 +0530

    Initial code setup

---
+++ gaugeseekbar/src/main/java/com/tenclouds/gaugeseekbar/DrawableEntity.kt
@@ -0,0 +1,6 @@
+package com.tenclouds.gaugeseekbar
+
+import android.graphics.PointF
+import android.graphics.drawable.Drawable
+
+abstract class DrawableEntity(protected var centerPosition: PointF) : Drawable()
